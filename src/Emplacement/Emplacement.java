36e1f6a87fd8f8bb4105e8e9c07c9761caeb8346
CBarneron <Corentin.barneron@gmail.com>
2022-10-26 23:42:34 +0200

    Tableau dans emplacement  fonctionnel , plus qu'a créer les methodes d'attribution

--- src/Emplacement/Emplacement.java
+++ src/Emplacement/Emplacement.java
@@ -1,33 +1,59 @@
 package Emplacement;
 
-public  class Emplacement{
+public  class Emplacement {
 
     /*Mas bibliothéque compte 5 ranger (ABCDE) avec chacune 5 étager comprenant 50 emplacement  donc 5**/
-    protected   int [][] etagereTab = new int[5][50];
-    protected String [] rangeTab= new String [2];
 
 
-    public int[][] getEtagereTab() {
-        return etagereTab;
+    protected String[][][] emplacemnt = new String[5][4][50];
+    private Integer ligne;// correspond au nbr rangée
+    private Integer etagere;// etagére comprise entre 1-4
+    private Integer place;// emplacement compris entre 0-49
+
+    public Integer getLigne() {
+        return ligne;
     }
 
-    public void setEtagerTab(int[][] emplacementTab) {
-        this.etagereTab = etagereTab;
+    public void setLigne(Integer ligne) {
+        this.ligne = ligne;
     }
 
-    public String[] getRangeTab() {
-        return rangeTab;
+    public Integer getEtagere() {
+        return etagere;
     }
 
-    public void setRangeTab(String[] rangeTab) {
-        this.rangeTab = rangeTab;
+    public void setEtagere(Integer etagere) {
+        this.etagere = etagere;
     }
-    /// instanciation d'emplacement
+
+    public Integer getPlace() {
+        return place;
+    }
+
+    public void setPlace(Integer place) {
+        this.place = place;
+    }
 
 
+    public static void Emplacement(String[] args) {
+        String[][][] e = new String[3][4][5];
 
-};
+        for (int l = 0; l < e.length ; l++) {
+            //setLigne(l)
+            for (int et = 1; et < e[l].length; et++) {
+                //setEtagere(i);
+                for (int p = 0; p < e[l][et].length ;p++) {
+                    //Setplacement(p)
+                    System.out.println("Emplacement[" + l + "," + et +" , "+ p+ "]=" + e[l][et][p]);
 
+
+                }
+            }
+        }
+
+
+    }
+}
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,19 @@ import Oeuvres.Ouvrages.Livres;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    Livres l1 = new Livres("test","test",2);
-    Emplacement E1 = new Emplacement("A",2,2);
-    System.out.println(l1);
-    System.out.println(E1);
-    //Livres.afficherLivres(1);
+        Livres l1 = new Livres("test", "test", 2);
+        //Emplacement E1 = new Emplacement("A",2,2);
+        System.out.println(l1);
+        //System.out.println(E1);
+        //Livres.afficherLivres(1);
 
-    Livres l2 = new Livres("TEST2","TEST2",2,1);
+        Livres l2 = new Livres("TEST2", "TEST2", 2, 1);
 
-    System.out.println(l2);
-    l2.rendreLivres();
-    l2.emprunterLivre();
-    l2.emprunterLivre();
+        System.out.println(l2);
+        l2.rendreLivres();
+        l2.emprunterLivre();
+        l2.emprunterLivre();
 
 
-
-}
-}
+        ;
+    }}
